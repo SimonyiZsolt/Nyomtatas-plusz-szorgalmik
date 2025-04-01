496583e4fdaa6a6aab1d06ffeccd7efd08db8d06
SimonyiZsolt <140444566+SimonyiZsolt@users.noreply.github.com>
2025-04-01 17:57:35 +0200

    Jav: Valasz megjelenites elott kell kiiratni a szamitottakat

--- F01_Nyomtats_megoldas/nyomtatas.js
+++ F01_Nyomtats_megoldas/nyomtatas.js
@@ -7,8 +7,9 @@ function kalkulal(){
     let terulet=Math.round((szelesseg*magassag)/10000);    
     let koltseg=terulet*papir;
     //Megjelenítés
-    document.getElementById('valasz').style.visibility = "visible";
     document.getElementById("terulet").innerHTML = terulet;
     document.getElementById("koltseg").innerHTML = koltseg;
+    document.getElementById('valasz').style.visibility = "visible";
+    
 }
 
